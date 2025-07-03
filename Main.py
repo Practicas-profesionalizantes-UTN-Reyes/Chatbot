b71ae9f81255d7986cb1d83e04f21dfa4345f558
dylan castiglioni <dylancastiglioni2015@gmail.com>
2025-07-03 11:53:26 -0300

    Obtenemos el los p y el titulo de la pagina y formateamos el contenido, luego lo guardamos en un Json

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
-#a
+import sys
+sys.path.append("./webscrapper")
+from titulo_parrafo import cont_pagina
+
+mostrar = (cont_pagina( "https://keepcoding.io/blog/"))
+for i in mostrar:
+    print(f"{i}:{mostrar[i]}\n")
